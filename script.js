4b26ef4f6fb2dc2603b3e2196c7423a6ba7c35fd
hailing045 <72068271+hailing045@users.noreply.github.com>
2021-01-03 22:19:41 +0000

    update related html file
    but not yet showing the chart properly

--- script.js
+++ script.js
@@ -47,3 +47,68 @@ function handleInput() {
         if (languageChart != null) languageChart;
     main(user, token);
 }
+
+async function main(user, token) {
+    var url = `https://api.github.com/users/${user}/repos`;
+    var repo = await getRequest(url, token).catch(error => console.error(error));
+
+    url = `https://api.github.com/users/${user}`;
+    var user_info = await getRequest(url, token).catch(error => console.error(error));
+
+    get_language_pie(repo, user, token);
+}
+
+async function getRequest(url, token) {
+    const headers = {
+        'Authorization': `Token ${token}`
+    }
+
+    const response = (token == undefined) ? await fetch(url) : await fetch(url, {
+        "method": "GET",
+        "headers": headers
+    });
+
+    let data = await response.json();
+    return data;
+}
+
+async function get_language_pie(repo, user, token) {
+    let data = [];
+    for (i in repo) {
+        let url = `https://api.github.com/repos/${user}/${repo[i].name}/languages`;
+        let languages = await getRequest(url, token).catch(error => console.error(error));
+    }
+    draw('language',data);
+}
+
+
+function draw(ctx,data) {
+
+    let myChart = document.getElementById(ctx).getContext('2d');
+
+    languageChart = new Chart(myChart, {
+        type: 'doughnut',
+        data: {
+            labels: label,
+            datasets: [{
+                backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+            }],
+        },
+    });
+}
