8ba24f0b1565df0e157c857b70db7c80bdc6dd89
hailing045 <72068271+hailing045@users.noreply.github.com>
2021-01-03 11:29:33 +0000

    update user profile area

--- script.js
+++ script.js
@@ -18,8 +18,22 @@ form.addEventListener('submit',function(e){
 
     document.getElementById("result").innerHTML = `
       <a target ="_blank" href = "https://github.com/${oName}">
-       <img src = "${data.avatar_url}"/>
+       <img src = "${data.avatar_url}" width="200" height="200"/>
       </a>
+      <div href = "https://github.com/${oName}">
+        <h6>User Profile</h6>
+        Bio: <i>${data.bio}</i>
+        <br>
+        Following: <i>${data.following}</i>
+        <br>
+        Followrs: <i>${data.followers}</i>
+        <br>
+        Date become a Github user: <i>${data.created_at}</i>
+
+
+      </div>
+      </body>
+
     `
   })
 })
