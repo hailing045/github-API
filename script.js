100d77367ebc9857ecdad8f5cffa2c5000de99e1
hailing045 <72068271+hailing045@users.noreply.github.com>
2021-01-03 22:56:59 +0000

    display the chart prettier
    define the colors and resize the chart

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ form.addEventListener('submit',function(e){
        <img src = "${data.avatar_url}" width="200" height="200"/>
       </a>
       <div href = "https://github.com/${oName}">
-        <h6>${oName} User Profile</h6>
+        <h3>${oName} User Profile</h3>
         Bio: <i>${data.bio==null?"He/She hasn't say anything":data.bio}</i>
         <br>
         Following: <i>${data.following==0?"He/She is not following anyone":data.following}</i>
@@ -74,15 +74,26 @@ async function getRequest(url, token) {
 
 async function get_language_pie(repo, user, token) {
     let data = [];
+    let label = [];
     for (i in repo) {
         let url = `https://api.github.com/repos/${user}/${repo[i].name}/languages`;
         let languages = await getRequest(url, token).catch(error => console.error(error));
+        for (language in languages) {
+            if (label.includes(language)) {
+                for (i = 0; i < label.length; i++)
+                    if (language == label[i])
+                        data[i] = data[i] + languages[language];
+            } else {
+                label.push(language);
+                data.push(languages[language]);
+            }
+        }
     }
-    draw('language',data);
+    draw('language', label,data);
 }
 
 
-function draw(ctx,data) {
+function draw(ctx, label,data) {
 
     let myChart = document.getElementById(ctx).getContext('2d');
 
@@ -91,24 +102,61 @@ function draw(ctx,data) {
         data: {
             labels: label,
             datasets: [{
+                data: data,
                 backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+
+                'rgba(45, 99, 102, 0.2)',
+                'rgba(211, 102, 255, 0.2)',
+                'rgba(144, 106, 86, 0.2)',
+                'rgba(35, 160, 152, 0.2)',
+                'rgba(122, 102, 155, 0.2)',
+                'rgba(57, 159, 64, 0.2)',
+                'rgba(95, 199, 202, 0.2)',
+                'rgba(211, 82, 25, 0.2)',
+                'rgba(14, 226, 86, 0.2)',
+                'rgba(35, 160, 222, 0.2)',
+                'rgba(122, 232, 155, 0.2)',
+                'rgba(97, 159, 24, 0.2)',
+                'rgba(214, 226, 186, 0.2)',
+                'rgba(135, 60, 22, 0.2)',
+                'rgba(252, 232, 155, 0.2)',
+                'rgba(97, 159, 224, 0.2)'
+
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+
+              'rgba(45, 99, 102, 0.2)',
+              'rgba(211, 102, 255, 0.2)',
+              'rgba(144, 106, 86, 0.2)',
+              'rgba(35, 160, 152, 0.2)',
+              'rgba(122, 102, 155, 0.2)',
+              'rgba(57, 159, 64, 0.2)',
+              'rgba(95, 199, 202, 0.2)',
+              'rgba(211, 82, 25, 0.2)',
+              'rgba(14, 226, 86, 0.2)',
+              'rgba(35, 160, 222, 0.2)',
+              'rgba(122, 232, 155, 0.2)',
+              'rgba(97, 159, 24, 0.2)',
+              'rgba(214, 226, 186, 0.2)',
+              'rgba(135, 60, 22, 0.2)',
+              'rgba(252, 232, 155, 0.2)',
+              'rgba(97, 159, 224, 0.2)'
             ],
             borderWidth: 1
             }],
         },
     });
 }
+var languageChart = null;
